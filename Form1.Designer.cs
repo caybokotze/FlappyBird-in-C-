6bc93cab0d162b25ace3e15d997b51d230401892
Langa <72982019+LangaXulu@users.noreply.github.com>
2020-10-28 17:28:32 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FlappyBird = new System.Windows.Forms.PictureBox();
+            this.PipeDown = new System.Windows.Forms.PictureBox();
+            this.PipeUp = new System.Windows.Forms.PictureBox();
+            this.Ground = new System.Windows.Forms.PictureBox();
+            this.MyTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FlappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FlappyBird
+            // 
+            this.FlappyBird.Image = global::WindowsFormsApp2.Properties.Resources.bird;
+            this.FlappyBird.Location = new System.Drawing.Point(92, 140);
+            this.FlappyBird.Name = "FlappyBird";
+            this.FlappyBird.Size = new System.Drawing.Size(48, 40);
+            this.FlappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FlappyBird.TabIndex = 0;
+            this.FlappyBird.TabStop = false;
+            // 
+            // PipeDown
+            // 
+            this.PipeDown.Image = global::WindowsFormsApp2.Properties.Resources.pipe;
+            this.PipeDown.Location = new System.Drawing.Point(159, 210);
+            this.PipeDown.Name = "PipeDown";
+            this.PipeDown.Size = new System.Drawing.Size(58, 172);
+            this.PipeDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeDown.TabIndex = 1;
+            this.PipeDown.TabStop = false;
+            // 
+            // PipeUp
+            // 
+            this.PipeUp.Image = global::WindowsFormsApp2.Properties.Resources.pipedown;
+            this.PipeUp.Location = new System.Drawing.Point(297, -5);
+            this.PipeUp.Name = "PipeUp";
+            this.PipeUp.Size = new System.Drawing.Size(58, 166);
+            this.PipeUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PipeUp.TabIndex = 2;
+            this.PipeUp.TabStop = false;
+            // 
+            // Ground
+            // 
+            this.Ground.Image = global::WindowsFormsApp2.Properties.Resources.ground;
+            this.Ground.Location = new System.Drawing.Point(0, 377);
+            this.Ground.Name = "Ground";
+            this.Ground.Size = new System.Drawing.Size(389, 73);
+            this.Ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ground.TabIndex = 3;
+            this.Ground.TabStop = false;
+            // 
+            // MyTimer
+            // 
+            this.MyTimer.Enabled = true;
+            this.MyTimer.Interval = 20;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(88, 33);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "Score:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(385, 450);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.Ground);
+            this.Controls.Add(this.PipeUp);
+            this.Controls.Add(this.PipeDown);
+            this.Controls.Add(this.FlappyBird);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.FlappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PipeUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox FlappyBird;
+        private System.Windows.Forms.PictureBox PipeDown;
+        private System.Windows.Forms.PictureBox PipeUp;
+        private System.Windows.Forms.PictureBox Ground;
+        private System.Windows.Forms.Timer MyTimer;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        int PipeSpeed = 8;
+        int gravity = 15;
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MyTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+
+                gravity = -15;
+            }
+        }
+
+        private void GameKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+
+                gravity = 15;
+            }
+        }
+        private void endGame()
+        {
+
+            MyTimer.Stop();
+            lblScore.Text += " Game over!!!";
+        }
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            FlappyBird.Top += gravity; 
+            PipeUp.Left -= PipeSpeed; 
+            PipeDown.Left -= PipeSpeed;
+            lblScore.Text = "Score: " + lblScore;
+
+            if (PipeDown.Left < -150)
+            {
+                
+                PipeDown.Left = 800;
+                score++;
+            }
+            if (PipeUp.Left < -180)
+            {
+                
+                PipeUp.Left = 950;
+                score++;
+            }
+
+          
+
+            if (FlappyBird.Bounds.IntersectsWith(PipeDown.Bounds) ||
+                FlappyBird.Bounds.IntersectsWith(PipeUp.Bounds) ||
+                FlappyBird.Bounds.IntersectsWith(Ground.Bounds) || FlappyBird.Top < -25
+                )
+            {
+                
+                endGame();
+            }
+
+
+            
+            if (score > 5)
+            {
+                PipeSpeed = 15;
+            }
+        }
+
+    }   
+}
